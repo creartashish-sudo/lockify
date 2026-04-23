5d071d0d7c0c70ce646c3bf31e86e287ba67898c
ashish <creart.ashish@gmail.com>
2026-04-23 15:41:38 +0530

    contact form change

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -166,7 +166,7 @@ console.log(apiUrl);
                     alert("New Password and Confirm Password do not match");
                     return;
                   }
-                  const res = await axios.post(`${apiUrl}/inquiries/create`, { ...values});
+                  const res = await axios.post(`${apiUrl}/inquiries/create`, { ...values,contact: String(values.contact) });
                   // console.log(res.data);
                   toast.success('Password changed successfully!');
                   navigate("/");
