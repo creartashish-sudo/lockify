9cdceb54b098a3a8ec02c5720cf01b023d68ff80
ashish <creart.ashish@gmail.com>
2026-04-24 13:02:12 +0530

    update cors changes

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ connectDB();
 // Middleware to parse JSON bodies
 
 app.use(cors({
-  origin: ["http://localhost:5173"], // your React URL
+  origin: ["*"], // your React URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
